9e6476c833160fcaf34b74d2c2e280e427396e72
Doron <dreinsager@gmail.com>
2018-11-08 11:31:06 -0600

    made changes

--- CSCI423/ProgrammingAssignment4/PartA.c
+++ CSCI423/ProgrammingAssignment4/PartA.c
@@ -5,20 +5,20 @@
 #include <stdlib.h>
 
 int i         = 0;	
-int ptsGenerated = 50000000;
-int totalInCircle  = 0;
+int pgen = 50000000;
+int total  = 0;
 
 void *count(void *X)
 { 
     
-    for (i=0; i < ptsGenerated; i++)
+    for (i=0; i < pgen; i++)
     {	
         double X = (double)rand() / RAND_MAX;
         double Y = (double)rand() / RAND_MAX;
         
         if (sqrt((X * X) + (Y * Y)) < 1.0)
         {
-            totalInCircle++;
+            total++;
         }
     }
     pthread_exit(0);
@@ -33,8 +33,8 @@ int main()
     pthread_create(&thread, NULL, &count, NULL);
     pthread_join(thread, NULL);
     
-    double points = 4.0 * totalInCircle;
-    double pi = points / ptsGenerated;
-    printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", ptsGenerated, pi);  
+    double points = 4.0 * total;
+    double pi = points / pgen;
+    printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", pgen, pi);  
     return 0;
 }
--- CSCI423/ProgrammingAssignment4/PartB.c
+++ CSCI423/ProgrammingAssignment4/PartB.c
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 
 int i         = 0;	
-int ptsGenerated = 50000000;
-int totalInCircle  = 0;
+int pgen = 50000000;
+int total  = 0;
 pthread_mutex_t lock;
 
 void *count(void *X)
 { 
     
-    for (i=0; i < ptsGenerated; i++)
+    for (i=0; i < pgen; i++)
     {	
 	pthread_mutex_lock(&lock);
         double X = (double)rand() / RAND_MAX;
@@ -20,7 +20,7 @@ void *count(void *X)
         
         if (sqrt((X * X) + (Y * Y)) < 1.0)
         {
-            totalInCircle++;
+            total++;
         }
 	pthread_mutex_unlock(&lock);
     }
@@ -31,17 +31,20 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-	printf("Please run as %s <number of threads>\n",argv[0]);
-	return 0;
+	printf("Usage: %s <number of threads>\n",argv[0]);
+	return -1;
 	}
+
 	if(argc > 2){
-	printf("Please run as %s <number of threads>\n",argv[0]);
-	return 0;
+	printf("Usage: %s <number of threads>\n",argv[0]);
+	return -1;
 	}
+
 int N=atoi(argv[1]);
+
 	if(N < 0){
 	printf("<number of threads> should be a positive integer\n");
-	return 0;
+	return -1;
 	}
     
   	pthread_t thread[N];
@@ -54,8 +57,8 @@ int N=atoi(argv[1]);
     	pthread_join(thread[i], NULL);
 	}
     
-    double points = 4.0 * totalInCircle;
-    double pi = points / ptsGenerated;
-    printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", ptsGenerated, pi);  
+    double points = 4.0 * total;
+    double pi = points / pgen;
+    printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", pgen, pi);  
     return 0;
 }
